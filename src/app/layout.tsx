643f79285a5fbc07dea648cf63835ed66d495441
Kunal Rana <kunalranarj2005@gmail.com>
2026-03-28 23:13:29 +0530

    added clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadcn } from "@clerk/ui/themes";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -26,11 +28,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", figtree.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider appearance={{ theme: shadcn }}>
+      <html
+        lang="en"
+        className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", figtree.variable)}
+      >
+        <body className="min-h-full flex flex-col">{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 /** @format */
+import { Button } from "@/components/ui/button";
+import { UserButton, SignOutButton } from "@clerk/nextjs";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+      <h1 className="text-4xl font-bold">Welcome to Creator Web</h1>
+      <p className="text-muted-foreground tracking-tight">
+        Authenticated with Clerk
+      </p>
+      <Button render={<SignOutButton>Sign Out</SignOutButton>}></Button>
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
