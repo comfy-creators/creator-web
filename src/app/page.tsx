e049466dbe6f6ce9ef3e841ef8523c8d74abd9d4
Kunal Rana <kunalranarj2005@gmail.com>
2026-03-29 11:23:14 +0530

    updated authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 /** @format */
+
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "@/lib/api";
 import { UserButton, SignOutButton } from "@clerk/nextjs";
 
 const Home = () => {
+  api.get("/me").then((res) => console.log(res.data));
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
       <h1 className="text-4xl font-bold">Welcome to Creator Web</h1>
